7e6ff220690178dff6fec522b62f934a6061afec
kiey <joan.marcual93@gmail.com>
2020-04-19 18:39:13 +0200

    initial

---
+++ main.py
@@ -0,0 +1,55 @@
+from selenium import webdriver
+import bs4
+import time
+import itertools
+
+#export PATH="$PATH:/home/joan/tests/"
+def get_webpage(url):
+	
+	driver = webdriver.Firefox('./')
+
+	driver.get(url)
+	time.sleep(1)
+	htmlSource = driver.page_source
+	driver.quit()
+
+
+	with open('iec_cantar.txt','w') as f:
+		f.write(str(htmlSource))
+
+def get_definitions(soup, examples=False):
+	"To do: implement tests, handle exempions word not found"
+	"""
+	Given the soup of the IEC webpage of a word it retrieves a list of the definitions.
+	If example is true, a list of tupples (definition,example) will be retrieved.
+	If a definition has no examples, the value returned is None.
+	"""
+	definitions_html = soup.find(class_="resultDefinition") # interesting part of the HTML
+	definitions_html = soup.find_all(name='span',class_="body") #list of HTML
+	count = 0
+	definitions_list = []
+	examples_list = []
+
+	for element in definitions_html:
+		words = element.text.split()
+		if all([x.isdigit() for x in words]):
+			count = 0
+		if count == 2:
+			definitions_list.append(element.text)
+		elif count == 3:
+			examples_list.append(element.text)
+		count += 1
+	if examples:
+		print(examples_list)
+		return itertools.zip_longest(definitions_list, examples_list)
+	return definitions_list
+
+if __name__ == '__main__':
+	word = "cantar"
+	url = f'https://dlc.iec.cat/results.asp?txtEntrada={word}'
+	get_webpage(url)
+	with open('iec_cantar.txt','r') as f:
+		soup = bs4.BeautifulSoup(f.read(), 'html.parser')
+	definitions = get_definitions(soup, examples=True)
+	for d,e in definitions:
+		print(d,e)
