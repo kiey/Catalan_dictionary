4102151050524aef91c8acf9f75fbc4f151b2228
kiey <joan.marcual.medina@gmail.com>
2020-04-20 17:34:45 +0200

    Working with 1 definition and bug solving
    changed ziplongest for zip and other changes. Examples were unordered and push to the begging of the list

--- src/catalanDictionary.py
+++ src/catalanDictionary.py
@@ -1,109 +1,76 @@
-from selenium import webdriver
 import bs4
-import time
 import itertools
 import exceptions
 import requests
 
+DEBUG = True
 
-# export PATH="$PATH:/home/joan/tests/"
-
-def get_soup_POST(url, word):
+def get_soup(url, word):
     html_get = requests.get(url)
     soup_get = bs4.BeautifulSoup(html_get.text, 'html.parser')
 
-    definitions_html = soup_get.find(class_="resultDefinition")  # interesting part of the HTML
+    with open('get_html.html', 'w') as f:
+        f.write(str(soup_get))
+
+    definitions_html = soup_get.find(class_="resultDefinition") 
     not_found = "No s'ha trobat cap entrada coincident amb els criteris de cerca"
     if (definitions_html.text == not_found):
         raise exceptions.WordNotFoundError(f'{word} not found')
 
-    id = soup_get.find(class_='resultAnchor')['id']
-    params = {'id': id, 'searchParam': word}
-    url_post = 'https://dlc.iec.cat/Results/Accepcio'
+    ids = [html['id'] for html in soup_get.find_all(class_='resultAnchor')]
+    soups = []
+    for id_ in ids:
+        params = {'id': id_, 'searchParam': word}
+        url_post = 'https://dlc.iec.cat/Results/Accepcio'
 
-    html_post = requests.post(url_post, data=params)
-    soup_post = bs4.BeautifulSoup(html_post.text, 'html.parser')
-    return soup_post
+        html_post = requests.post(url_post, data=params)
+        soup_post = bs4.BeautifulSoup(html_post.text, 'html.parser')
+        if DEBUG:
+            with open(f'../logs/{word}{id_}.html', 'w') as f:
+                f.write(soup_post.  prettify())
+        soups.append(soup_post)
 
-
-def get_webpage(url):
-    driver = webdriver.Firefox('./')
-    driver.get(url)
-
-    time.sleep(1)  # wait for javascript to run
-
-    htmlSource = driver.page_source
-    driver.quit()
-
-    return(str(htmlSource))  # To Do: check if cast to string is necessary
+    return soups
 
 
-def scrap_definitions(soup, word, examples=False):
+def scrap_definitions(soups, word, examples=False):
     """
     Given the soup of the IEC webpage of a word it retrieves a list of the definitions.
     If example is true, a list of tupples (definition,example) will be retrieved.
     If a definition has no examples, the value returned is None.
     """
-    definitions_html = soup.find(class_="resultDefinition")  # interesting part of the HTML
-    not_found = "No s'ha trobat cap entrada coincident amb els criteris de cerca"
-    if (definitions_html.text == not_found):
-        raise exceptions.WordNotFoundError(f'{word} not found')
-    definitions_html = soup.find_all(name='span', class_="body")  # list of HTML
-    count = 0
     definitions_list = []
     examples_list = []
+    for soup in soups:
+        definitions_html = soup.find_all(name='span', class_="\\\"body\\\"")  # list of HTML
+        count = 1
 
-    for element in definitions_html:
-        words = element.text.split()
-        if all([x.isdigit() for x in words]):
-            count = 0
-        if count == 2:
-            definitions_list.append(element.text)
-        elif count == 3:
-            examples_list.append(element.text)
-        count += 1
+        for element in definitions_html:
+            words = element.text.split()
+            if all([x.isdigit() for x in words]):
+                count = 0
+            if count == 2:
+                definitions_list.append(element.text)
+                examples_list.append(None)
+            elif count == 3:
+                examples_list[-1] = element.text
+            count += 1
     if examples:
-        return itertools.zip_longest(definitions_list, examples_list)
-    return definitions_list
-
-
-def scrap_definitions_POST(soup, word, examples=False):
-    """
-    Given the soup of the IEC webpage of a word it retrieves a list of the definitions.
-    If example is true, a list of tupples (definition,example) will be retrieved.
-    If a definition has no examples, the value returned is None.
-    """
-
-    definitions_html = soup.find_all(name='span', class_="\\\"body\\\"")  # list of HTML
-    count = 0
-    definitions_list = []
-    examples_list = []
-
-    for element in definitions_html:
-        words = element.text.split()
-        if all([x.isdigit() for x in words]):
-            count = 0
-        if count == 2:
-            definitions_list.append(element.text)
-        elif count == 3:
-            examples_list.append(element.text)
-        count += 1
-    if examples:
-        return list(itertools.zip_longest(definitions_list, examples_list))
+        assert(len(examples_list) == len(definitions_list))
+        return list(zip(definitions_list, examples_list))
     else:
         return definitions_list
 
 
 def get_definitions(word, examples=False):
     url = f'https://dlc.iec.cat/results.asp?txtEntrada={word}'
-    soup_post = get_soup_POST(url, word)
-    definitions = scrap_definitions_POST(soup_post, word, examples)
+    soups = get_soup(url, word)
+    definitions = scrap_definitions(soups, word, examples)
     return definitions
 
 
 if __name__ == "__main__":
-    word = "cantar"
+    word = "bicicleta"
     definitions_list = get_definitions(word, examples=True)
     for d, e in definitions_list:
-        print(e)
-
+        print(d, e)
--- tests/test.py
+++ tests/test.py
@@ -10,10 +10,31 @@ import catalanDictionary
 import exceptions 
 
 
+def test_bicicleta():
+    word = 'bicicleta'
+    definitions = catalanDictionary.get_definitions(word)
+    assert(len(definitions) == 1)
+    print(definitions[0])
+    assert(definitions[0] == 'Vehicle lleuger de dues rodes, unides per un quad'
+                             're, la del davant directora i la del darrere m'
+                             'otora, que s’acciona amb pedals. ')
+
+
+def test_bicicleta_exemples():
+    word = 'bicicleta'
+    definitions = catalanDictionary.get_definitions(word, examples=True)
+    assert(len(definitions) == 1)
+    assert(definitions[0][0] == 'Vehicle lleuger de dues rodes, unides per un quad'
+                                're, la del davant directora i la del darrere m'
+                                'otora, que s’acciona amb pedals. ')
+    assert(definitions[0][1] == 'Bicicleta de muntanya.')
+
+
 def test_cantar():
     word = 'cantar'
     definitions = catalanDictionary.get_definitions(word)
-    assert(len(definitions) == 20)
+    assert(len(definitions) == 21)
+    assert(all(definitions))
 
 
 def test_cantar_examples():
@@ -21,13 +42,27 @@ def test_cantar_examples():
     definitions = catalanDictionary.get_definitions(word, examples=True)
     definitions_unzipped = [[i for i, j in definitions],
                             [j for i, j in definitions]]
-    assert(len(definitions_unzipped[0]) == 20)
-    assert(len(definitions_unzipped[1]) == 20)
-    assert(all(definitions_unzipped[1]))
+    assert(len(definitions_unzipped[0]) == 21)
+    assert(all(definitions_unzipped[0]))
+    assert(len(definitions_unzipped[1]) == 21)
+    assert(any(definitions_unzipped[1]))
+    assert(definitions[19][0] == 'Fer pudor. ')
+    for d, e in definitions:
+        print(d, e)
+    assert(definitions[19][1] == 'Li canten els peus.')
+
+
+def test_cartera_examples():
+    word = 'cartera'
+    definitions = catalanDictionary.get_definitions(word, examples=True)
+    definitions_unzipped = [[i for i, j in definitions],
+                            [j for i, j in definitions]]
+    assert(len(definitions_unzipped[0]) == 12)
+    assert(len(definitions_unzipped[1]) == 12)
+    assert(any(definitions_unzipped[1]))
 
 
 def test_WordNotFoundError():
-    word = "abcdff"
+    word = "abcdf"
     with pytest.raises(exceptions.WordNotFoundError):
         definitions = catalanDictionary.get_definitions(word, examples=True)
-
