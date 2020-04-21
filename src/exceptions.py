1d3995f16d574e8985b8b3872a70fff78a98bde2
kiey <joan.marcual.medina@gmail.com>
2020-04-21 23:22:25 +0200

    Added custom exceptions

--- src/exceptions.py
+++ src/exceptions.py
@@ -1,2 +1,14 @@
 class WordNotFoundError(Exception):
-	pass
+    def __init__(self, word):
+        self.message = f'{word} not found'
+
+    def __str__(self):
+        return self.message
+
+
+class EmptyStrError(Exception):
+    def __init__(self, message=''):
+        self.message = 'Word parameter is an empty string'
+
+    def __str__(self):
+        return self.message
